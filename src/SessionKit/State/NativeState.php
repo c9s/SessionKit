d70bc8563f49940744f73f8bf4c972a7a1469869
c9s <cornelius.howl@gmail.com>
2014-02-04 23:23:48 +0800

    fixing session expiry

--- src/SessionKit/State/NativeState.php
+++ src/SessionKit/State/NativeState.php
@@ -8,9 +8,12 @@ class NativeState
     {
         if ( ! isset($_SESSION) ) {
             if ( isset($_REQUEST['session_expiry']) ) {
-                session_set_cookie_params($_REQUEST['session_expiry']); // 1 month
+                session_set_cookie_params(intval($_REQUEST['session_expiry'])); // 1 month
             }
             session_start();
+            if ( isset($_REQUEST['session_expiry']) ) {
+                setcookie(session_name(),session_id(),time()+ intval($_REQUEST['session_expiry']) );
+            }
         }
     }
 
